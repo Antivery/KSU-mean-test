42364cf90ae1bba2247557ff581da1a3f88740b8
Antivery <antivery@gmail.com>
2025-02-15 14:47:16 -0500

    profile editor add angular mat added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileEditorComponent } from './componets/profile-editor/profile-editor.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ProfileEditorComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mean-test';
+  name = 'Anthony Ivery'
+  date = '01/26/2025'
+  formName = 'Profile Editor'
 }
